ab6059641afa0a8df79a5628204c98d4ccd128d1
Jigish Patel <tskraja@gmail.com>
2024-10-20 05:18:25 -0700

    translate aci_uid to aci_executor for originator

--- pkg/thread/thread.go
+++ pkg/thread/thread.go
@@ -97,7 +97,7 @@ func (t *Thread) append(typ EntryType, originator []byte, message string) *Threa
 	t.Context = append(t.Context, &Entry{
 		ID:         len(t.Context),
 		Type:       typ,
-		Originator: originator,
+		Originator: getOriginator(originator),
 		Message:    message,
 	})
 
@@ -115,6 +115,30 @@ func (t *Thread) append(typ EntryType, originator []byte, message string) *Threa
 	}
 
 	return t
+}
+
+// HACK: not enough time to pull this from aci registry
+func getOriginator(uid []byte) []byte {
+	uidHex := Hex(uid)
+	var originator []byte
+	var err error
+	if uidHex == "0xaf3a33c2f95a9e41e54c386aad4d260b2f3fe73a73353d3c439871bbf2301e41" {
+		originator, err = FromHex("0x980b3a841817e0c7cbfc921bfccf796fa84c80f3")
+	} else if uidHex == "0xa3f374f49528ef97f2c6adad7931e87373782d5e9b965de2e61554828275a033" {
+		originator, err = FromHex("0x7b5d5f79934f4995a5d9214b6701e98e903d26c7")
+	} else if uidHex == "0x822434c25a9837f0e7244090c1558663dee097f16f7623f0bf461c8afee4c55b" {
+		originator, err = FromHex("0x17f5e274c2c8aa9471e5320cb68df543db90d083")
+	} else if uidHex == "0xeaac656a5054ef4a92f34da8870b97a7a3037f20181ad169956b4a631903d466" {
+		originator, err = FromHex("0xc04c0b99b0b50e61f6a71f43e18871803c2ba2af")
+	} else {
+		originator = uid
+	}
+
+	if err != nil {
+		log.Err(err).Msg("error getting originator")
+	}
+
+	return originator
 }
 
 func (t *Thread) Append(typ EntryType, originator []byte, message string) {
@@ -145,7 +169,7 @@ func Request(uid []byte, parentUID []byte, aciUID []byte, requester []byte, requ
 				{
 					ID:         0,
 					Type:       EntryType_Request,
-					Originator: requester,
+					Originator: getOriginator(requester),
 					Message:    requestRef, // TODO: ref -> actual request
 				},
 			},
